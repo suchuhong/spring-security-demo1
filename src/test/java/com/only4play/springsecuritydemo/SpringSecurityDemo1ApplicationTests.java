727613ce4d6f3f0b72c889d7d3a6216c66cffd9f
suchuhong <1442615457@qq.com>
2024-07-28 15:02:05 +0800

    init

---
+++ src/test/java/com/only4play/springsecuritydemo/SpringSecurityDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.only4play.springsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
